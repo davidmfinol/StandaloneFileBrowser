9417e6b74d53cf11a0365dc9f7bff8633eb149ec
David Finol <davidmfinol@gmail.com>
2022-03-18 20:09:46 -0500

    Use Assets from other project

--- Assets/StandaloneFileBrowser/Sample/CanvasSampleOpenFileTextMultiple.cs
+++ Assets/StandaloneFileBrowser/Sample/CanvasSampleOpenFileTextMultiple.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using SFB;
+using UnityEngine.Networking;
 
 [RequireComponent(typeof(Button))]
 public class CanvasSampleOpenFileTextMultiple : MonoBehaviour, IPointerDownHandler {
@@ -52,10 +53,11 @@ public class CanvasSampleOpenFileTextMultiple : MonoBehaviour, IPointerDownHandl
 
     private IEnumerator OutputRoutine(string[] urlArr) {
         var outputText = "";
-        for (int i = 0; i < urlArr.Length; i++) {
-            var loader = new WWW(urlArr[i]);
+        foreach (string url in urlArr)
+        {
+            UnityWebRequest loader = UnityWebRequest.Get(url);
             yield return loader;
-            outputText += loader.text;
+            outputText += loader.downloadHandler.text;
         }
         output.text = outputText;
     }
